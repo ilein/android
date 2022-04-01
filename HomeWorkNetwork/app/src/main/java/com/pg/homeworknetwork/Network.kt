4de1bccd4949a2a0b4e27a00ab2ed8fce7c5aef8
ilein <ilein2005@inbox.ru>
2022-04-01 17:23:48 +0500

    Bobrova Lyudmila (HomeWorkNetwork)

--- HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/Network.kt
+++ HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/Network.kt
@@ -1,5 +1,56 @@
 package com.pg.homeworknetwork
 
-class Api {
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
 
+class Api {
+    private val baseUrl = "api.themoviedb.org"
+    private val movies = "${BuildConfig.API_VERSION}/movie/popular"
+    private val movie = "${BuildConfig.API_VERSION}/movie/"
+
+    val ktorLogger = object : Logger {
+        override fun log(message: String) {
+            if (message.contains("api_key=")) {
+                println("${message.split("=").first()}=BuildConfig.API_KEY")
+            } else {
+                println(message)
+            }
+        }
+    }
+
+    private val ktorClient = HttpClient() {
+        install(Logging) {
+            logger = ktorLogger
+            level = LogLevel.BODY
+        }
+
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    suspend fun getMovie(movieId: Int): Movie = ktorClient.use {
+        it.get(
+            host = baseUrl,
+            path = movie + movieId.toString()
+        ) {
+            parameter("api_key", BuildConfig.API_KEY)
+        }
+    }
+
+    suspend fun getMovies(): Movies = ktorClient.use {
+        it.get(
+            host = baseUrl,
+            path = movies
+        ) {
+            parameter("api_key", BuildConfig.API_KEY)
+        }
+    }
 }
